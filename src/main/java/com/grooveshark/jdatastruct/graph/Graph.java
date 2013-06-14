d8e02400e5f84342be81c468beb55b9f82315360
Anandan <andy.compeer@gmail.com>
2013-06-14 15:06:22 -0400

    Change all dos fileformat to unix

--- src/main/java/com/grooveshark/jdatastruct/graph/Graph.java
+++ src/main/java/com/grooveshark/jdatastruct/graph/Graph.java
@@ -10,7 +10,9 @@ public class Graph
 
     public static final Logger log = Logger.getLogger(Graph.class);
 
+    protected Vector<Node> vertices = new Vector<Node>();
+    protected Vector<Edge> edges = new Vector<Edge>();
+
     public Graph() {
-        Vector<Node> vertices = new Vector<Node>();
     }
 }
