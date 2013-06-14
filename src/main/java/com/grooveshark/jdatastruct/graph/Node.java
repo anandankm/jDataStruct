64d242cdbccc8954a4b96de834ce04b5b5a7fa25
Anandan <andy.compeer@gmail.com>
2013-06-14 18:38:06 -0400

    Visited, predecessor, distance parameters

--- src/main/java/com/grooveshark/jdatastruct/graph/Node.java
+++ src/main/java/com/grooveshark/jdatastruct/graph/Node.java
@@ -13,6 +13,8 @@ public class Node<T extends Comparable<T>> implements Comparable<Node<T>>
     protected HashSet<Node<T>> adjNodes = new HashSet<Node<T>>();
 
     protected double distance = Double.POSITIVE_INFINITY;
+    protected boolean visited;
+    protected Node<T> predecessor = null;
 
     public Node() {
     }
@@ -23,5 +25,74 @@ public class Node<T extends Comparable<T>> implements Comparable<Node<T>>
 
     public int compareTo(Node<T> o) {
         return this.value.compareTo(o.value);
+    }
+
+    public boolean equals(Node<T> o) {
+        return this.value.equals(o.value);
+    }
+
+    public String toString() {
+        return "value: " + this.value.toString();
+    }
+
+    /**
+     * Setter method for distance
+     */
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+    /**
+     * Getter method for distance
+     */
+    public double getDistance() {
+        return this.distance;
+    }
+    /**
+     * Setter method for visited
+     */
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+    /**
+     * Getter method for visited
+     */
+    public boolean getVisited() {
+        return this.visited;
+    }
+    /**
+     * Setter method for value
+     */
+    public void setValue(T value) {
+        this.value = value;
+    }
+    /**
+     * Getter method for value
+     */
+    public T getValue() {
+        return this.value;
+    }
+    /**
+     * Setter method for adjNodes
+     */
+    public void setAdjNodes(HashSet<Node<T>> adjNodes) {
+        this.adjNodes = adjNodes;
+    }
+    /**
+     * Getter method for adjNodes
+     */
+    public HashSet<Node<T>> getAdjNodes() {
+        return this.adjNodes;
+    }
+    /**
+     * Setter method for predecessor
+     */
+    public void setPredecessor(Node<T> predecessor) {
+        this.predecessor = predecessor;
+    }
+    /**
+     * Getter method for predecessor
+     */
+    public Node<T> getPredecessor() {
+        return this.predecessor;
     }
 }
