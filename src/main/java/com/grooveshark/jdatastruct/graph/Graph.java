a6ec218c894ab02c67d7e4cf7004b90ca9cc1d2a
Anandan <andy.compeer@gmail.com>
2013-06-15 16:01:42 -0400

    Use treemap of vertices. Retrieval while inserting is faster

--- src/main/java/com/grooveshark/jdatastruct/graph/Graph.java
+++ src/main/java/com/grooveshark/jdatastruct/graph/Graph.java
@@ -2,29 +2,40 @@ package com.grooveshark.jdatastruct.graph;
 
 
 import java.util.Vector;
+import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 
-public class Graph
+public class Graph<T extends Comparable>
 {
 
     public static final Logger log = Logger.getLogger(Graph.class);
 
-    protected Vector<Node> vertices;
+    protected TreeMap<T, Node> vertices;
     protected Vector<Edge> edges;
 
+    protected boolean directed = false;
+
     public Graph() {
-        this.vertices = new Vector<Node>();
+        this.vertices = new TreeMap<T, Node>();
         this.edges = new Vector<Edge>();
     }
 
-    public boolean addNode(Node n) {
-        this.vertices.add(n);
-        return true;
+    public Node addNode(T v) {
+        if (this.vertices.containsKey(v)) {
+            return this.vertices.get(v);
+        }
+        Node n = new Node(v);
+        this.vertices.put(v, n);
+        return n;
     }
 
     public boolean addEdge(Edge e) {
         this.edges.add(e);
+        e.addBToA();
+        if (!this.directed) {
+            e.addAToB();
+        }
         return true;
     }
 
@@ -32,5 +43,18 @@ public class Graph
         return "Graph<Vertices, Edges>: <" +
             this.vertices.toString() + ", " +
             this.edges.toString();
+    }
+
+    /**
+     * Setter method for directed
+     */
+    public void setDirected(boolean directed) {
+        this.directed = directed;
+    }
+    /**
+     * Getter method for directed
+     */
+    public boolean getDirected() {
+        return this.directed;
     }
 }
