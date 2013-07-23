ddc901c0cd0f9df51263642584b4b743fdfb9c68
Anandan <andy.compeer@gmail.com>
2013-07-23 15:51:17 -0400

    Add a sample enum in route to access int, string info in a single call

--- src/main/java/com/grooveshark/jdatastruct/graph/enums/Column.java
+++ src/main/java/com/grooveshark/jdatastruct/graph/enums/Column.java
@@ -1,5 +1,16 @@
 package com.grooveshark.jdatastruct.graph.enums;
 
-public enum Column 
-{
+public enum Column {
+    userid (0, Integer.class),
+    username (1, String.class),
+    fname (3, String.class),
+    lname (4, String.class),
+    mname (5, String.class);
+
+    public int num;
+    public Class<?> type;
+
+    Column(int num, Class<?> type) {
+        this.num = num;
+    }
 }
