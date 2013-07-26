c3068aecf58a65ddd6b1e9e1fc7d3a4bdf9f0c8e
Anandan <andy.compeer@gmail.com>
2013-07-26 17:57:50 -0400

    Add a tester for Neo4jRest server setup

--- src/test/java/com/grooveshark/jdatastruct/graph/sample/SampleNodeTest.java
+++ src/test/java/com/grooveshark/jdatastruct/graph/sample/SampleNodeTest.java
@@ -5,21 +5,48 @@ import org.junit.Test;
 import static org.junit.Assert.fail;
 
 import org.apache.log4j.Logger;
+import com.grooveshark.util.StringUtils;
 
 public class SampleNodeTest
 {
-    public static final Logger log = Logger.getLogger(SampleNodeTest.class); 
+    public static final Logger log = Logger.getLogger(SampleNodeTest.class);
 
     public static final String nodeFile = "data/node_file";
+    public static final String locationFile = "data/location_file";
+
     @Test
     public void testNodeParser() {
         try {
-            NodeParser parser = new NodeParser(nodeFile);
+            NodeParser parser = new NodeParser(nodeFile, locationFile);
             parser.displayNodes();
             parser.displayEdges();
         } catch (Exception ex) {
             ex.printStackTrace();
             fail("Failed to test node parser: " + ex.getMessage());
+        }
+    }
+
+    public void testNodeFields() {
+        try {
+            System.out.println("Fields in Node class: " + StringUtils.getFieldNames(GNode.class));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Failed to test node fields: " + ex.getMessage());
+        }
+    }
+
+    public void testNeo4jRest() {
+        try {
+            String serverUri = "http://localhost:7474/db/data";
+            String nodeInd = "users";
+            String nodeKey = "userid";
+            String indexTextKey = "indexText";
+            Neo4jRest server = new Neo4jRest(serverUri, nodeInd);
+            server.setNodeKey(nodeKey);
+            server.setIndexTextKey(indexTextKey);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Failed to test neo4j rest instantiation: " + ex.getMessage());
         }
     }
 
