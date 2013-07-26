fc249b24c816e6db797858205d3eea3b3157c28f
Anandan <andy.compeer@gmail.com>
2013-07-26 17:56:17 -0400

    Parse locations file and add lat/long info

--- src/main/java/com/grooveshark/jdatastruct/graph/sample/NodeParser.java
+++ src/main/java/com/grooveshark/jdatastruct/graph/sample/NodeParser.java
@@ -15,29 +15,70 @@ public class NodeParser
 
     public static final Logger log = Logger.getLogger(NodeParser.class);
 
-    public List<Node> nodes = new LinkedList<Node>();
-    public List<Edge> edges = new LinkedList<Edge>();
+    public List<GNode> nodes = new LinkedList<GNode>();
+    public List<GEdge> edges = new LinkedList<GEdge>();
     public List<String> nodeFileLines;
+    public List<String> locationLines;
 
     public HashMap<String, Integer> friendsToBase = new HashMap<String, Integer>();
-    public Node baseNode;
+    public HashMap<String, String> locationMap = new HashMap<String, String>();
+    public GNode baseNode;
 
-    public NodeParser(String nodeFile) throws IOException {
+    public Location baseLocation;
+    public Location closeLocation;
+    public Location farLocation;
+
+    public class Location {
+        String city;
+        Float latitude;
+        Float longitude;
+    }
+
+    public NodeParser(String nodeFile, String locationFile) throws IOException {
+        this.locationLines = FileUtils.readFile(locationFile);
         this.nodeFileLines = FileUtils.readFile(nodeFile);
+        this.parseLocationFile();
         this.parseNodes();
+    }
+
+    public void parseLocationFile() {
+        for (String line : this.locationLines) {
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] lineSplits = line.split("\t");
+            String desc = lineSplits[0].trim();
+            if (desc.equalsIgnoreCase("Base") ||
+                desc.equalsIgnoreCase("Close") ||
+                desc.equalsIgnoreCase("Far"))
+            {
+                Location location = new Location();
+                location.city = lineSplits[1].trim();
+                location.latitude = Float.parseFloat(lineSplits[2].trim());
+                location.longitude = Float.parseFloat(lineSplits[3].trim());
+                if (desc.equalsIgnoreCase("Base")) {
+                    this.baseLocation = location;
+                } else if (desc.equalsIgnoreCase("Close")) {
+                    this.closeLocation = location;
+                } else if (desc.equalsIgnoreCase("Far")) {
+                    this.farLocation = location;
+                }
+
+            }
+        }
     }
 
     public void displayNodes() {
         System.out.println("Nodes:");
-        for (Node node : this.nodes) {
+        for (GNode node : this.nodes) {
             System.out.println(node.toString());
         }
     }
 
-
     public void displayEdges() {
         System.out.println("Edges:");
-        for (Edge edge : this.edges) {
+        for (GEdge edge : this.edges) {
             System.out.println(edge.toString());
         }
     }
@@ -53,17 +94,19 @@ public class NodeParser
             if (StringUtils.isInteger(splits[0])) {
                 int userid = Integer.parseInt(splits[0]);
                 String username = splits[1].trim();
-                Node node = new Node(userid, username);
+                GNode node = new GNode(userid, username);
                 setFakeProperties(node);
                 this.nodes.add(node);
                 String[] usernameSplits = node.username.split(" ");
-                if (usernameSplits.length == 1) {
+                int uslen = usernameSplits.length;
+                if (uslen == 1) {
                     this.baseNode = node;
-                } else if (usernameSplits.length == 3) {
+                    this.setLocation(node, this.baseLocation);
+                } else if (uslen == 3) {
                     String type = usernameSplits[1].trim();
                     this.createEdges(type, this.baseNode.userid, userid);
                     this.friendsToBase.put(username, userid);
-                } else if (usernameSplits.length == 5) {
+                } else if (uslen == 5) {
                     String friendsUname = usernameSplits[0] + " " +
                                           usernameSplits[1] + " " +
                                           usernameSplits[2];
@@ -72,29 +115,41 @@ public class NodeParser
                         this.createEdges(usernameSplits[3].trim(), friendsUserid, userid);
                     }
                 }
+                if (uslen > 1) {
+                    if (usernameSplits[uslen-1].equalsIgnoreCase("Close")) {
+                        this.setLocation(node, this.closeLocation);
+                    } else if (usernameSplits[uslen-1].equalsIgnoreCase("Far")) {
+                        this.setLocation(node, this.farLocation);
+                    }
+                }
             } else {
                 continue;
             }
         }
     }
 
+    public void setLocation(GNode node, Location location) {
+        node.city = location.city;
+        node.latitude = location.latitude;
+        node.longitude = location.longitude;
+    }
+
     public void createEdges(String type, int sUserid, int eUserid) {
-        Edge edge = null;
+        GEdge edge = null;
         if (type.equalsIgnoreCase("Mutual") || type.equalsIgnoreCase("Following")) {
-            edge = new Edge(sUserid, eUserid);
+            edge = new GEdge(sUserid, eUserid);
             this.edges.add(edge);
         }
         if (type.equalsIgnoreCase("Mutual") || type.equalsIgnoreCase("Follower")) {
-            edge = new Edge(eUserid, sUserid);
+            edge = new GEdge(eUserid, sUserid);
             this.edges.add(edge);
         }
     }
 
-    public void setFakeProperties(Node node) {
+    public void setFakeProperties(GNode node) {
         node.fname =  node.userid + " First Name";
         node.lname =  node.userid + " Last Name";
         node.mname =  node.userid + " Middle Name";
-        node.city =  node.userid + " City";
         node.state =  node.userid + " State";
         node.country =  node.userid + " Country";
         node.zip =  node.userid + " zip";
