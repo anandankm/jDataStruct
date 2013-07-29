0d39036aaaf6428921d48d42bd7b212a88ad4d5d
Anandan <andy.compeer@gmail.com>
2013-07-29 11:06:03 -0400

    Initialize location properties

--- src/main/java/com/grooveshark/jdatastruct/graph/sample/entities/Location.java
+++ src/main/java/com/grooveshark/jdatastruct/graph/sample/entities/Location.java
@@ -1,10 +1,11 @@
 package com.grooveshark.jdatastruct.graph.sample.entities;
 
-public class Location 
+public class Location
 {
-    public String city;
+    public String city = "";
+    public String state = "";
+    public String country = "";
+    public String zip = "";
     public Float latitude;
     public Float longitude;
-    public String state;
-    public String country;
 }
