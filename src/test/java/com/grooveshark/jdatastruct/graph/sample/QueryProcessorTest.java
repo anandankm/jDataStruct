5581c51d429b38861d58205034883ddfa57bbf75
Anandan <andy.compeer@gmail.com>
2013-07-29 17:03:37 -0400

    Add tests to use cypher queries and simple queries.

--- src/test/java/com/grooveshark/jdatastruct/graph/sample/QueryProcessorTest.java
+++ src/test/java/com/grooveshark/jdatastruct/graph/sample/QueryProcessorTest.java
@@ -20,22 +20,47 @@ public class QueryProcessorTest
     public static final String NODE_KEY = "userid";
     public static final String REL_KEY = "edge";
     public static final Map<String, Object> REL_PROPS = map("name", "follows");
+    private Neo4jRest server;
+    private QueryProcessor processor;
 
-
-    @Test
-    public void testQuery() {
+    @Before
+    public void setup() {
         try {
             System.out.println("Setting up server");
-            Neo4jRest server = new Neo4jRest(SERVER_URI, NODE_INDEX, REL_INDEX);
-            server.setNodeKey(NODE_KEY);
-            server.setRelKey(REL_KEY);
-            server.setRelProps(REL_PROPS);
-            QueryProcessor processor = new QueryProcessor(1, "username:*oni*", server);
+            this.server = new Neo4jRest(SERVER_URI, NODE_INDEX, REL_INDEX);
+            this.server.setNodeKey(NODE_KEY);
+            this.server.setRelKey(REL_KEY);
+            this.server.setRelProps(REL_PROPS);
+            int baseUserid = 1;
+            String indexQuery = "username:*oni*";
+            this.processor = new QueryProcessor(baseUserid, indexQuery, this.server);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Failed to test neo4j rest instantiation: " + ex.getMessage());
+        }
+    }
+
+    public void testSimpleQuery() {
+        try {
             long start = System.currentTimeMillis();
             System.out.println("Querying");
-            processor.processQuery();
-            float elapsed = (System.currentTimeMillis() - start)/(float) 1000;
-            System.out.println("Done. [" + elapsed + " secs]");
+            this.processor.traverseOneDeg();
+            float elapsed = System.currentTimeMillis() - start;
+            System.out.println("Done. [" + elapsed + " ms]");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Failed to test neo4j rest instantiation: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCypherQuery() {
+        try {
+            long start = System.currentTimeMillis();
+            System.out.println("Querying");
+            this.processor.traverseCypher();
+            float elapsed = System.currentTimeMillis() - start;
+            System.out.println("Done. [" + elapsed + " ms]");
         } catch (Exception ex) {
             ex.printStackTrace();
             fail("Failed to test neo4j rest instantiation: " + ex.getMessage());
